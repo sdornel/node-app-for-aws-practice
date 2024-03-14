f2ab3d0cba2f3470fd9c838ad90f0f5c43e253eb
sdornel <kai_dornel@hotmail.com>
2024-03-14 12:32:13 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Handle POST requests to /api/data
+app.post('/api/data', (req, res) => {
+    const inputData = req.body.data;
+    console.log('Received data:', inputData);
+
+    // Process the data and send a response
+    res.send(`Received data: ${inputData}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
